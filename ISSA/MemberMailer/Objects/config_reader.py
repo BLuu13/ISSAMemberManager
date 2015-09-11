f44b90191f333773dffdf6701535204ab31d771a
Brian Luu <brianluu12@gmail.com>
2015-09-10 19:34:30 -0700

    Added functions

--- ISSA/MemberMailer/Objects/config_reader.py
+++ ISSA/MemberMailer/Objects/config_reader.py
@@ -1,4 +1,4 @@
-__author__ = 'Sebastian'
+_author__ = 'Sebastian'
 
 import xml.etree.ElementTree as ET
 import os
@@ -6,30 +6,41 @@ import sys
 
 class ConfigReader():
 
-    data_file = "E:/Users/Sebastian/Desktop/sample_config2.xml"
+    # data_file = "E:/Users/Sebastian/Desktop/sample_config2.xml
+    config_location = '.\config\membermailer.xml'
 
-    if os.path.exists(data_file) is not True:
-        sys.exit("Error: Path is incorrect or file does not exist")
+    def __init__(self):
+        try:
+            self.config = ET.parse(self.config_location)
+            self.read_config()
+            self.print_root()
+            self.print_parent()
+        except ET.ParseError as error:
+            # todo: something to pull down generic membermailer.xml from web
+            sys.exit("Error: Path is incorrect or file does not exist")
 
-    else:
-        # Read from an xml file
-        tree = ET.parse(data_file)
-        root = tree.getroot()
+    def read_config(self):
+        root = self.config.getroot()
+      # self.database - get db file location from config
 
+    def print_root(self):
         print("------------------------------")
         print("Printing Root Element...")
         print("------------------------------")
-        print(root.tag)
+        print(self.root.tag)
 
+    def print_parent(self):
         print("------------------------------")
         print("Printing Parent Elements...")
         print("------------------------------")
         for child in root:
                 print(child.tag)
-
+    
+    def print_children(self):
         print("------------------------------")
         print("Printing Children Elements...")
         print("------------------------------")
         for child in root:
             for children in child:
                 print("Key:", children.tag, "-> Value:", children.text)
+
